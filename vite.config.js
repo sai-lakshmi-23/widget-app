667a509becd3ea27b660600e8f4b7b057bb2f905
sai-lakshmi-23 <Sailakshmivasantha@gmail.com>
2024-09-05 23:54:50 +0530

    added config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/widget-app/",
-  build: {
-    outDir: 'dist',
-  },
+  base: "/widget-app/"
 })
